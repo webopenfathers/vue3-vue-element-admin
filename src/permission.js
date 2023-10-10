b61e3fe4e8667f0265fb32968069a267147d09e7
webopenfathers <1290443925@qq.com>
2023-10-10 20:52:24 +0800

    feat(鉴权): 2023 10-10 20:52 登录鉴权解决方案

---
+++ src/permission.js
@@ -0,0 +1,29 @@
+import router from '@/router'
+import store from './store'
+
+// 白名单
+const whiteList = ['/login']
+
+/**
+ * 路由前置守卫
+ * @param {*} to 要到那里去
+ * @param {*} from 从哪里来
+ * @param {*} next 是否要去?
+ */
+router.beforeEach((to, from, next) => {
+  if (store.getters.token) {
+    // 1.用户已登陆，则不允许进入 login
+    if (to.path === '/login') {
+      next('/')
+    } else {
+      next()
+    }
+  } else {
+    // 2.用户未登录，只允许进入 login
+    if (whiteList.indexOf(to.path) > -1) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
