ee3a62db98d3daadfec2a657a0847a490a310fd6
webopenfathers <1290443925@qq.com>
2023-10-10 21:28:55 +0800

    feat(layout架构): 2023 10-10 21:28 创建基于 Layout 的基础架构-1

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,7 +1,40 @@
 <template>
-  <div>layout 页面</div>
+  <div class="app-wrapper">
+    <!-- 左侧 menu -->
+    <sidebar class="sidebar-container"></sidebar>
+    <div class="main-container">
+      <div class="fixed-header">
+        <!-- 顶部 navbar -->
+        <navbar></navbar>
+      </div>
+      <!-- 内容区 -->
+      <app-main></app-main>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Navbar from './components/Navbar.vue'
+import Sidebar from './components/Sidebar/index.vue'
+import AppMain from './components/AppMain.vue'
+</script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import '~@/styles/mixin.scss';
+@import '~@/styles/variables.scss';
+
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.fixed-header {
+  position: fixed;
+  top: 0;
+  right: 0;
+  z-index: 9;
+  width: calc(100% - #{$sideBarWidth});
+}
+</style>
