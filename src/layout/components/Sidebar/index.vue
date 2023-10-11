fd7386f78872fca18f8e675ab8ddb3f290bc1dc8
webopenfathers <1290443925@qq.com>
2023-10-11 21:58:51 +0800

    feat(菜单): 2023 10-11 21:58 创建页面组件，使用临时 menu 菜单

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>SideBar</div>
+  <div>
+    <h1>占位</h1>
+    <el-scrollbar>
+      <sidebar-menu>s</sidebar-menu>
+    </el-scrollbar>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import SidebarMenu from './SidebarMenu.vue'
+</script>
 
 <style lang="scss" scoped></style>
