870fb3f3dcb03b420c7e12e6c1595458c0e5e6d5
webopenfathers <1290443925@qq.com>
2023-10-11 20:54:07 +0800

    feat(头像): 2023 10-11 20:53 渲染用户头像菜单

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,7 +1,58 @@
 <template>
-  <div>Navbar</div>
+  <div class="navbar">
+    <div class="right-menu">
+      <!-- 头像 -->
+      <el-dropdown class="avatar-container" trigger="click">
+        <div class="avatar-wrapper">
+          <el-avatar
+            shape="square"
+            :size="40"
+            :src="$store.getters.userInfo.avatar"
+          ></el-avatar>
+          <i class="el-icon-s-tools"></i>
+        </div>
+        <template #dropdown>
+          <el-dropdown-menu class="user-dropdown">
+            <router-link to="/">
+              <el-dropdown-item>主页</el-dropdown-item>
+            </router-link>
+            <a target="_blank" href="#">
+              <el-dropdown-item>课程主页</el-dropdown-item>
+            </a>
+            <el-dropdown-item divided>退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.navbar {
+  height: 50px;
+  overflow: hidden;
+  position: relative;
+  background-color: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+  .right-menu {
+    display: flex;
+    align-items: center;
+    float: right;
+    padding-right: 16px;
+    ::v-deep .avatar-container {
+      cursor: pointer;
+      .avatar-wrapper {
+        margin-top: 5px;
+        position: relative;
+        .el-avatar {
+          // 去掉avatar组件的背景色
+          --el-avatar-background-color: none;
+          margin-right: 12px;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/store/getters.js
+++ src/store/getters.js
@@ -7,7 +7,8 @@ const getters = {
    */
   hasUserInfo: (state) => {
     return JSON.stringify(state.user.userInfo) !== '{}'
-  }
+  },
+  userInfo: (state) => state.user.userInfo
 }
 
 export default getters
