2acc9635849cffbbfa37af2cf723885f867844ef
webopenfathers <1290443925@qq.com>
2023-10-11 21:45:36 +0800

    feat(退出): 2023 10-11 21:45 用户被动退出解决方案之被动处理

--- src/utils/request.js
+++ src/utils/request.js
@@ -50,6 +50,14 @@ service.interceptors.response.use(
   },
   // 请求失败
   (error) => {
+    // token 过期
+    if (
+      error.response &&
+      error.response.data &&
+      error.response.data.code === 401
+    ) {
+      store.dispatch('user/logout')
+    }
     ElMessage.error(error.message)
     return Promise.reject(error)
   }
