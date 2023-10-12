18b8c69b35c8806c4bef9c54bd44cc18468ab207
webopenfathers <1290443925@qq.com>
2023-10-12 21:24:17 +0800

    feat(路由表): 2023 10-12 21:24 创建结构路由表

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,125 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
+import layout from '@/layout/index.vue'
 
+/**
+ * 定义 路由表 对应 menu 菜单规则
+ * 对于单个路由规则而言（循环）：
+   一：如果meta && meta.title && meta.icon ：则显示在 menu 菜单中(其中 title 为显示的内容，icon 为显示的图标)
+   1. 如果存在 children ：则以 el-sub-menu（子菜单） 展示
+   2. 否则：则以 el-menu-item（菜单项） 展示
+
+   二：否则：不显示在 menu 菜单中
+ */
+
+/**
+ * 私有路由表
+ */
+const privateRoutes = [
+  // user相关
+  {
+    path: '/user',
+    redirect: '/user/manage',
+    component: layout,
+    meta: {
+      title: 'user',
+      icon: 'personnel'
+    },
+    children: [
+      {
+        path: '/user/manage',
+        name: 'userManage',
+        component: () => import('@/views/user-manage/index.vue'),
+        meta: {
+          title: 'userManage',
+          icon: 'personnel-manage'
+        }
+      },
+      {
+        path: '/user/role',
+        name: 'userRole',
+        component: () => import('@/views/role-list/index.vue'),
+        meta: {
+          title: 'roleList',
+          icon: 'role'
+        }
+      },
+      {
+        path: '/user/permission',
+        name: 'userPermission',
+        component: () => import('@/views/permission-list/index.vue'),
+        meta: {
+          title: 'permissionList',
+          icon: 'permission'
+        }
+      },
+      {
+        path: '/user/info/:id',
+        name: 'userInfo',
+        component: () => import('@/views/user-info/index.vue'),
+        meta: {
+          title: 'userInfo'
+        }
+      },
+      {
+        path: '/user/import',
+        name: 'import',
+        component: () => import('@/views/import/index.vue'),
+        meta: {
+          title: 'excelImport'
+        }
+      }
+    ]
+  },
+  // article相关
+  {
+    path: '/article',
+    component: layout,
+    redirect: '/article/ranking',
+    meta: {
+      title: 'article',
+      icon: 'article'
+    },
+    children: [
+      {
+        path: '/article/ranking',
+        name: 'articleRanking',
+        component: () => import('@/views/article-ranking/index'),
+        meta: {
+          title: 'articleRanking',
+          icon: 'article-ranking'
+        }
+      },
+      {
+        path: '/article/:id',
+        name: 'articleDetail',
+        component: () => import('@/views/article-detail/index'),
+        meta: {
+          title: 'articleDetail'
+        }
+      },
+      {
+        path: '/article/create',
+        name: 'articleCreate',
+        component: () => import('@/views/article-create/index'),
+        meta: {
+          title: 'articleCreate',
+          icon: 'article-create'
+        }
+      },
+      {
+        path: '/article/editor/:id',
+        name: 'articleEditor',
+        component: () => import('@/views/article-create/index'),
+        meta: {
+          title: 'articleEditor'
+        }
+      }
+    ]
+  }
+]
+/**
+ * 公开路由表
+ */
 const publicRoutes = [
   {
     path: '/login',
@@ -7,13 +127,38 @@ const publicRoutes = [
   },
   {
     path: '/',
-    component: () => import('@/layout/index.vue')
+    redirect: '/profile',
+    component: layout,
+    children: [
+      // 个人中心
+      {
+        path: '/profile',
+        name: 'profile',
+        component: () => import('@/views/profile/index.vue'),
+        meta: {
+          title: 'profile',
+          icon: 'el-icon-user'
+        }
+      },
+      // 404
+      {
+        path: '/404',
+        name: '404',
+        component: () => import('@/views/error-page/404.vue')
+      },
+      // 401
+      {
+        path: '/401',
+        name: '401',
+        component: () => import('@/views/error-page/401.vue')
+      }
+    ]
   }
 ]
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes: publicRoutes
+  routes: [...publicRoutes, ...privateRoutes]
 })
 
 export default router
