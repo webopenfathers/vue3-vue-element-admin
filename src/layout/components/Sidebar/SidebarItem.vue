313544692372e01ebea418a5115d2295167e1192
webopenfathers <1290443925@qq.com>
2023-10-13 19:45:42 +0800

    feat(生成menu): 2023 10-13 19:45 生成动态menu菜单

---
+++ src/layout/components/Sidebar/SidebarItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <el-submenu v-if="route.children.length" :index="route.path">
+    <template #title>
+      <!-- 渲染文本和图标 -->
+      <menu-item :title="route.meta.title" :icon="route.meta.icon"></menu-item>
+    </template>
+    <!-- 循环渲染 -->
+    <sidebar-item
+      v-for="item in route.children"
+      :key="item.path"
+      :route="item"
+    ></sidebar-item>
+  </el-submenu>
+
+  <el-menu-item v-else :index="route.path">
+    <!-- 渲染文本和图标 -->
+    <menu-item :title="route.meta.title" :icon="route.meta.icon"></menu-item>
+  </el-menu-item>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+import MenuItem from './MenuItem.vue'
+defineProps({
+  route: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
+<style lang="scss" scoped></style>
--- src/layout/components/Sidebar/SidebarMenu.vue
+++ src/layout/components/Sidebar/SidebarMenu.vue
@@ -5,27 +5,18 @@
     text-color="#fff"
     active-text-color="#ffd04b"
   >
-    <!-- 子集 menu---存在children -->
-    <el-submenu index="1">
-      <template #title>
-        <i class="el-icon-location"></i>
-        <span>导航一</span>
-      </template>
-      <!--  -->
-      <el-menu-item index="1-1">选项一</el-menu-item>
-      <el-menu-item index="1-2">选项二</el-menu-item>
-    </el-submenu>
-    <!-- 具体菜单---不存在children -->
-    <el-menu-item index="2">
-      <i class="el-icon-location"></i>
-      <template #title> 导航4 </template>
-    </el-menu-item>
+    <sidebar-item
+      v-for="item in routes"
+      :key="item.path"
+      :route="item"
+    ></sidebar-item>
   </el-menu>
 </template>
 
 <script setup>
 import { useRouter } from 'vue-router'
 import { filterRoutes, generateMenus } from '@/utils/route'
+import SidebarItem from './SidebarItem.vue'
 import { computed } from 'vue'
 const router = useRouter()
 
