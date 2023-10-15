8e0155551539659e4168fb87480d9c270476ecd4
webopenfathers <1290443925@qq.com>
2023-10-15 20:24:30 +0800

    feat(立即生效): 2023 10-15 20:24 element-plus 新主题的立即生效

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
   <router-view />
 </template>
 
-<script setup></script>
+<script setup>
+import { useStore } from 'vuex'
+import { generateNewStyle, writeNewStyle } from '@/utils/theme'
+
+// 解决刷新以后主题失效的原因---head头中重新写入样式
+const store = useStore()
+generateNewStyle(store.getters.mainColor).then((newStyle) => {
+  writeNewStyle(newStyle)
+})
+</script>
 
 <style></style>
