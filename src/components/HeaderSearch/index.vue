c518d4ea3b282581480e041a0963409046b36ae0
webopenfathers <1290443925@qq.com>
2023-10-19 21:02:31 +0800

    feat(bugfix): 2023 10-19 21:02 剩余问题处理

--- src/components/HeaderSearch/index.vue
+++ src/components/HeaderSearch/index.vue
@@ -25,11 +25,12 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { ref, computed, watch } from 'vue'
 import { filterRoutes } from '@/utils/route'
 import { generateRoutes } from './FuseData'
 import { useRouter } from 'vue-router'
 import Fuse from 'fuse.js'
+import { watchSwitchLang } from '@/utils/i18n'
 
 // 数据源
 const router = useRouter()
@@ -42,32 +43,56 @@ console.log(searchPool.value)
 // 搜索库相关
 // 参数一:数据源searchPool
 // 参数二:配置对象
-const fuse = new Fuse(searchPool.value, {
-  // 是否按优先级进行排序
-  shouldSort: true,
-  // 匹配长度超过这个值的才会被认为是匹配的
-  minMatchCharLength: 1,
-  // 将被搜索的键列表.支持嵌套路径,加权搜索,在字符串和对象数组中搜索.
-  // name:搜索的键
-  // weight:对应的权重
-  keys: [
-    {
-      name: 'title',
-      weight: 0.7
-    },
-    {
-      name: 'path',
-      weight: 0.3
-    }
-  ]
-})
-console.log(fuse)
+let fuse
+const initFuse = (searchPool) => {
+  fuse = new Fuse(searchPool.value, {
+    // 是否按优先级进行排序
+    shouldSort: true,
+    // 匹配长度超过这个值的才会被认为是匹配的
+    minMatchCharLength: 1,
+    // 将被搜索的键列表.支持嵌套路径,加权搜索,在字符串和对象数组中搜索.
+    // name:搜索的键
+    // weight:对应的权重
+    keys: [
+      {
+        name: 'title',
+        weight: 0.7
+      },
+      {
+        name: 'path',
+        weight: 0.3
+      }
+    ]
+  })
+}
+initFuse(searchPool)
 
 // 控制 search 展示
 const isShow = ref(false)
 const onShowClick = () => {
-  console.log(999)
   isShow.value = !isShow.value
+}
+
+watch(isShow, (val) => {
+  if (val) {
+    // search 输入框为打开状态
+    headerSearchSelectRef.value.focus()
+    document.body.addEventListener('click', onClose)
+  } else {
+    // search 输入框为关闭状态
+    document.body.removeEventListener('click', onClose)
+  }
+})
+
+// 关闭事件
+const headerSearchSelectRef = ref(null)
+const onClose = () => {
+  // 失去焦点
+  headerSearchSelectRef.value.blur()
+  // 关闭和清空
+  isShow.value = false
+  searchOptions.value = []
+  search.value = ''
 }
 
 // search 相关
@@ -86,6 +111,11 @@ const querySearch = (query) => {
 const onSelectChange = (val) => {
   router.push(val.path)
 }
+
+// 监听到语言变化--重新回调初始化Fuse
+watchSwitchLang(() => {
+  initFuse(searchPool)
+})
 </script>
 
 <style lang="scss" scoped>
--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -1,5 +1,19 @@
 import i18n from '@/i18n'
+import { watch } from 'vue'
+import store from '@/store'
 // 处理 sidebar左侧菜单 与 面包屑 区域的国际化
 export function generateTitle(title) {
   return i18n.global.t('msg.route.' + title)
+}
+
+/**
+ * 监听语言变化,同时执行 cb,cb可以时多个参数
+ */
+export function watchSwitchLang(...cbs) {
+  watch(
+    () => store.getters.language,
+    () => {
+      cbs.forEach((cb) => cb(store.getters.language))
+    }
+  )
 }
