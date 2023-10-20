2e42303611f35dd504f7463b273aeebef5d68cac
webopenfathers <1290443925@qq.com>
2023-10-20 22:08:46 +0800

    feat(tagsView): 2023 10-20 生成 tagsView

--- src/store/getters.js
+++ src/store/getters.js
@@ -24,7 +24,9 @@ const getters = {
   // 国际化
   language: (state) => state.app.language,
   // 主题色
-  mainColor: (state) => state.theme.mainColor
+  mainColor: (state) => state.theme.mainColor,
+  // tags
+  tagsViewList: (state) => state.app.tagsViewList
 }
 
 export default getters
