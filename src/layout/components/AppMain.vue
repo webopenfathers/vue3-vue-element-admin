07bd9ac09831cdff6b2896dd4a61fc7c909e6b5c
webopenfathers <1290443925@qq.com>
2023-10-21 10:37:16 +0800

    feat(动态过渡): 2023 10-21 10:37 处理基于路由的动态过渡

--- src/layout/components/AppMain.vue
+++ src/layout/components/AppMain.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="app-main">
-    <router-view></router-view>
+    <!-- 带有切换动画，并且具备组件缓存的routerView -->
+    <router-view v-slot="{ Component, route }">
+      <transition name="fade-transform" mode="out-in">
+        <!-- 也可指定 include属性指定某些页面需要缓存，其他不需要 -->
+        <keep-alive>
+          <component :is="Component" :key="route.path" />
+        </keep-alive>
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -67,11 +75,11 @@ watchSwitchLang(() => {
 <style lang="scss" scoped>
 .app-main {
   // 浏览器可视区域的高度 100vh
-  min-height: calc(100vh - 50px);
+  min-height: calc(100vh - 50px - 43px);
   width: 100%;
   position: relative;
   overflow: hidden;
-  padding: 61px 20px 20px 20px;
+  padding: 104px 20px 20px 20px;
   box-sizing: border-box;
 }
 </style>
