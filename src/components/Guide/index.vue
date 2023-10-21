c84f837ebabea9338821c78cdb4b229ef921bf53
webopenfathers <1290443925@qq.com>
2023-10-21 22:02:06 +0800

    feat(guide): 2023 10-21 22:01 生成 Guide

---
+++ src/components/Guide/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <el-tooltip :content="$t('msg.navBar.guide')">
+      <svg-icon icon="guide"></svg-icon>
+    </el-tooltip>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -5,6 +5,8 @@
     <!-- 面包屑 -->
     <Breadcrumb class="breadcrumb-container" />
     <div class="right-menu">
+      <!-- guide引导 -->
+      <guide class="right-menu-item hover-effect" />
       <!-- 搜索 -->
       <HeaderSearch class="right-menu-item hover-effect" />
       <!-- 全屏 -->
@@ -49,6 +51,7 @@ import LangSelect from '@/components/LangSelect'
 import ThemeSelect from '@/components/ThemeSelect'
 import ScreenFull from '@/components/ScreenFull'
 import HeaderSearch from '@/components/HeaderSearch'
+import Guide from '@/components/Guide'
 
 const store = useStore()
 const logout = () => {
