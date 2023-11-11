d8e070c287856b24d4d9d47c743e1e8e7ddc014d
webopenfathers <1290443925@qq.com>
2023-11-11 22:03:18 +0800

    feat(excel导出时间): 2023 11-11 22:03 业务落地:excel导出时的时间逻辑处理

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,6 +1,6 @@
 import dayjs from 'dayjs'
 
-const dateFilter = (val, format = 'YYYY-MM-DD') => {
+export const dateFilter = (val, format = 'YYYY-MM-DD') => {
   // 可以被转成数字
   if (!isNaN(val)) {
     val = parseInt(val)
--- src/views/user-manage/components/Export2Excel.vue
+++ src/views/user-manage/components/Export2Excel.vue
@@ -26,6 +26,7 @@ import { useI18n } from 'vue-i18n'
 import { watchSwitchLang } from '@/utils/i18n'
 import { getUserManageAllList } from '@/api/user-manage'
 import { USER_RELATIONS } from './Export2ExcelConstants'
+import { dateFilter } from '@/filters'
 defineProps({
   modeValue: {
     type: Boolean,
@@ -79,6 +80,10 @@ const formatJson = (headers, rows) => {
       if (headers[key] === 'role') {
         const roles = item[headers[key]]
         return JSON.stringify(roles.map((role) => role.title))
+      }
+      // 利用dayjs进行--时间处理
+      if (headers[key] === 'openTime') {
+        return dateFilter(item[headers[key]])
       }
       return item[headers[key]]
     })
