1b37d4c75e4bcfdb5ea6698b77059ea996d723ce
webopenfathers <1290443925@qq.com>
2023-11-11 21:49:10 +0800

    feat(excel导出): 2023 11-11 21:50 实现 excel 导出逻辑

--- src/views/user-manage/components/Export2Excel.vue
+++ src/views/user-manage/components/Export2Excel.vue
@@ -25,6 +25,7 @@ import { defineProps, defineEmits, ref } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { watchSwitchLang } from '@/utils/i18n'
 import { getUserManageAllList } from '@/api/user-manage'
+import { USER_RELATIONS } from './Export2ExcelConstants'
 defineProps({
   modeValue: {
     type: Boolean,
@@ -52,9 +53,36 @@ const closed = () => {
 const loading = ref(false)
 const onConfirm = async () => {
   loading.value = true
+  // 后端返回全部列表数据下载
   const { list } = await getUserManageAllList()
-  console.log(list)
+  // 导入工具包--动态导入
+  const excel = await import('@/utils/Export2Excel')
+  const data = formatJson(USER_RELATIONS, list)
+  excel.export_json_to_excel({
+    // excel 表头
+    header: Object.keys(USER_RELATIONS),
+    // excel 数据
+    data,
+    // 文件名称
+    filename: excelName.value || exportDefaultName
+    // 是否自动列宽--已默认具有
+    // 文件类型--已默认xlsx
+  })
   closed()
+}
+
+// 当使用 export_json_to_excel 的时候，我们传递的 data 数据，它必须是一个二维数组
+const formatJson = (headers, rows) => {
+  return rows.map((item) => {
+    return Object.keys(headers).map((key) => {
+      // 角色需要进行特殊处理
+      if (headers[key] === 'role') {
+        const roles = item[headers[key]]
+        return JSON.stringify(roles.map((role) => role.title))
+      }
+      return item[headers[key]]
+    })
+  })
 }
 </script>
 
