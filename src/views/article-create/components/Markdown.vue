a343b908276094468273638d516ba28ac48ff1d4
webopenfathers <1290443925@qq.com>
2023-12-03 17:39:13 +0800

    feat(markdown实现): 2023 12-3 17:38 新建文章:markdown 实现

--- src/views/article-create/components/Markdown.vue
+++ src/views/article-create/components/Markdown.vue
@@ -1,5 +1,60 @@
 <template>
-  <div class="">markdown</div>
+  <div class="markdown-container">
+    <!-- 渲染区 -->
+    <div id="markdown-box"></div>
+    <!-- 按钮 -->
+    <div class="bottom">
+      <el-button type="primary" @click="onSubmitClick">{{
+        $t('msg.article.commit')
+      }}</el-button>
+    </div>
+  </div>
 </template>
-<script setup></script>
-<style scoped lang="scss"></style>
+<script setup>
+import MKEditor from '@toast-ui/editor'
+import '@toast-ui/editor/dist/toastui-editor.css'
+import '@toast-ui/editor/dist/i18n/zh-cn'
+import { useStore } from 'vuex'
+import { onMounted } from 'vue'
+import { watchSwitchLang } from '@/utils/i18n'
+
+// Editor 实例
+let mkEditor
+let el
+
+onMounted(() => {
+  el = document.querySelector('#markdown-box')
+  initEditor()
+})
+
+const store = useStore()
+const initEditor = () => {
+  mkEditor = new MKEditor({
+    // el
+    el,
+    // 高度
+    height: '500px',
+    // 样式
+    previewStyle: 'vertical',
+    // 国际化
+    language: store.getters.language === 'zh' ? 'zh-CN' : 'en'
+  })
+  mkEditor.getMarkdown()
+}
+
+watchSwitchLang(() => {
+  if (!el) return true
+  const htmlStr = mkEditor.getHTML()
+  mkEditor.destroy()
+  initEditor()
+  mkEditor.setHTML(htmlStr)
+})
+</script>
+<style scoped lang="scss">
+.markdown-container {
+  .bottom {
+    margin-top: 20px;
+    text-align: right;
+  }
+}
+</style>
