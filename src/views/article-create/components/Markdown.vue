56e9619fdf220711925b8590f87fb9df2681b5ef
webopenfathers <1290443925@qq.com>
2023-12-03 17:01:45 +0800

    feat(创建文章): 2023 12-3 17:01 创建文章基本结构实现

---
+++ src/views/article-create/components/Markdown.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="">markdown</div>
+</template>
+<script setup></script>
+<style scoped lang="scss"></style>
--- src/views/article-create/index.vue
+++ src/views/article-create/index.vue
@@ -1,7 +1,37 @@
 <template>
-  <div class="">创建文章</div>
+  <div class="article-create">
+    <el-card>
+      <el-input
+        class="title-input"
+        :placeholder="$t('msg.article.titlePlaceholder')"
+        v-model="title"
+        maxlength="20"
+        clearable
+      ></el-input>
+
+      <el-tabs v-model="activeName">
+        <el-tab-pane :label="$t('msg.article.markdown')" name="markdown">
+          <Markdown />
+        </el-tab-pane>
+        <el-tab-pane :label="$t('msg.article.richText')" name="editor">
+          <Editor />
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import Editor from './components/Editor.vue'
+import Markdown from './components/Markdown.vue'
 
-<style lang="scss" scoped></style>
+const title = ref('')
+const activeName = ref('markdown')
+</script>
+
+<style lang="scss" scoped>
+.title-input {
+  margin-bottom: 20px;
+}
+</style>
