eaf08dcdc0de1a0010a08797be2271921e21187e
webopenfathers <1290443925@qq.com>
2023-12-04 20:22:43 +0800

    feat(element-plus升级): 2023 12-4 20:22 element-plus升级的国际化问题

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-  <router-view />
+  <el-config-provider :locale="store.getters.language === 'en' ? en : zhCn">
+    <router-view />
+  </el-config-provider>
 </template>
 
 <script setup>
 import { useStore } from 'vuex'
 import { generateNewStyle, writeNewStyle } from '@/utils/theme'
 import { watchSwitchLang } from './utils/i18n'
+import zhCn from 'element-plus/dist/locale/zh-cn'
+import en from 'element-plus/dist/locale/en'
 
 // 解决刷新以后主题失效的原因---head头中重新写入样式
 const store = useStore()
